8a64cb5f2a67cea36e9b9a9161edd055be07a823
Lîm Tsú-thuàn <dannypsnl@gmail.com>
2023-01-26 22:03:17 +0800

    add index
    Signed-off-by: Lîm Tsú-thuàn <dannypsnl@gmail.com>

--- render-emacs-config-to-html.sh
+++ render-emacs-config-to-html.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 
-emacs ./topology.org --batch -Q -f org-html-export-to-html --kill
+for VARIABLE in $(ls *.org)
+do
+emacs ${VARIABLE} --batch -Q -f org-html-export-to-html --kill
+done
 
 # output will be the directory uploaded to the render repository so we have to put all the resulting files inside that folder
 mkdir -p dist
