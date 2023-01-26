704975001af3f5a38be317b9c8cb368b0c5804c4
Lîm Tsú-thuàn <dannypsnl@gmail.com>
2023-01-26 21:58:09 +0800

    rebuild
    Signed-off-by: Lîm Tsú-thuàn <dannypsnl@gmail.com>

--- render-emacs-config-to-html.sh
+++ render-emacs-config-to-html.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-emacs topology.org --batch -Q --load org-render-html-minimal.el -f org-html-export-to-html --kill
+emacs ./topology.org --batch -Q -f org-html-export-to-html --kill
 
 # output will be the directory uploaded to the render repository so we have to put all the resulting files inside that folder
-mkdir dist
+mkdir -p dist
 mv *.html dist/
