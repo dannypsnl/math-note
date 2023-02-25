1e3a887430568d3aebe10e6666be86a4a2f549c8
Lîm Tsú-thuàn <dannypsnl@gmail.com>
2023-02-26 02:20:05 +0800

    add build config & update something
    Signed-off-by: Lîm Tsú-thuàn <dannypsnl@gmail.com>

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 
 for VARIABLE in $(ls *.org)
 do
-emacs ${VARIABLE} --batch -Q -f org-html-export-to-html --kill
+emacs -l ./build.el ${VARIABLE} --batch -Q -f org-html-export-to-html --kill
 done
 
 # output will be the directory uploaded to the render repository so we have to put all the resulting files inside that folder
