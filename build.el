a9b8c342afcc17fe1682fd698a3af566478ca5a6
Lîm Tsú-thuàn <dannypsnl@gmail.com>
2023-02-26 02:21:11 +0800

    config for html output
    Signed-off-by: Lîm Tsú-thuàn <dannypsnl@gmail.com>

--- build.el
+++ build.el
@@ -1,19 +1,3 @@
-(setq org-latex-compiler "xelatex")
-(setq org-latex-pdf-process
-      (list (concat "latexmk -"
-                    org-latex-compiler
-                    " -recorder -synctex=1 -bibtex-cond %b")))
-(setq org-latex-listings t)
-(setq org-latex-default-packages-alist
-      '(("" "graphicx" t)
-        ("" "grffile" t)
-        ("" "longtable" nil)
-        ("" "wrapfig" nil)
-        ("" "rotating" nil)
-        ("normalem" "ulem" t)
-        ("" "amsmath" t)
-        ("" "textcomp" t)
-        ("" "amssymb" t)
-        ("" "capt-of" nil)
-        ("" "hyperref" nil)
-        ("UTF8" "ctex" t)))
+(setq org-html-head-include-default-style nil)
+(setq org-html-htmlize-output-type 'css)
+(setq org-src-preserve-indentation t)
