6725766b5fab8125d1b83cf2b0397f1e07f9330c
Lîm Tsú-thuàn <dannypsnl@gmail.com>
2023-03-09 02:09:07 +0800

    build pdf
    Signed-off-by: Lîm Tsú-thuàn <dannypsnl@gmail.com>

---
+++ buildpdf/config.el
@@ -0,0 +1,19 @@
+(setq org-latex-compiler "xelatex")
+(setq org-latex-pdf-process
+      (list (concat "latexmk -"
+                    org-latex-compiler
+                    " -recorder -synctex=1 -bibtex-cond %b")))
+(setq org-latex-listings t)
+(setq org-latex-default-packages-alist
+      '(("" "graphicx" t)
+        ("" "grffile" t)
+        ("" "longtable" nil)
+        ("" "wrapfig" nil)
+        ("" "rotating" nil)
+        ("normalem" "ulem" t)
+        ("" "amsmath" t)
+        ("" "amsthm" t)
+        ("" "textcomp" t)
+        ("" "amssymb" t)
+        ("" "capt-of" nil)
+        ("" "hyperref" nil)))
